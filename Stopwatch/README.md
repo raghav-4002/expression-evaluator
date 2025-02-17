23584deb94213e45b2a81ee357ce60ba052f2c54
Raghav Sharma <code.practice17@gmail.com>
2025-02-16 22:26:51 -0500

    Added user input and Updated README

---
+++ Stopwatch/README.md
@@ -0,0 +1,36 @@
+# A CLI based Stopwatch in C
+<br>
+
+## What is this?
+- A CLI based Stopwatch, written entirely in C.
+
+<br>
+
+## What it does?
+- Show the time elapsed since the execution of the program.
+- Stops the program when user types any character
+- Executes the program in non-canonical mode.
+    - Thus no need to press enter when typing the character.
+- Uses 'read()' syscall in order to read the character.
+    - read() is a low-level syscall.
+    - It allows finer control over user-input and output.
+
+<br>
+
+## Potential bugs and missing features:
+- Bugs
+    - It seems to me that the stopwatch is little faster than expected (not totally sure).
+
+- Missing features:
+    - Only compiles on POSIX Compliant operating systems (Unix, Linux and MacOs)
+
+<br>
+
+## What I learned from this project?
+- Low-level terminal interface and it's manipulation.
+    - Learned about canonical and non-canonical terminal modes.
+    - Learned about the 'read()' syscall.
+    - Learned about 'File streams and file descriptors' and how they differ.
+    - Learned about 'bit-flags, bit-manipulation and bit-masking'.
+
+- 'time.h' and time functions and datatypes available in C.
--- Stopwatch/main.c
+++ Stopwatch/main.c
@@ -1,29 +1,90 @@
-#include <stdio.h>
-#include <time.h>
-#include <unistd.h>
+/* 
+ * 'termios.h', 'time.h' and 'unistd.h' header files are only available 
+ * in POSIX Compliant Operating Systems, like Unix and Unix-like OSes (including Linux and MacOs)
+ * Thus this program won't run on Windows (as it is not POSIX Compliant).
+ * In future, I will try to add compatibility for Windows also.
+ */
 
-void clrscr(void);
+#include <termios.h> /*Only in POSIX*/
+#include <unistd.h>  /*Only in POSIX*/
+#include <time.h>    /*Only in Posix*/
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void enable_raw_mode(void);
+void disable_raw_mode(void);
+void clear_screen(void);
 void format_time(time_t seconds);
+
+
+/*Hold original terminal attributes*/
+struct termios orig_termios;
+
 
 int main(void)
 {
+    /*Enable non-canonical mode*/
+    enable_raw_mode();
 
-    struct timespec start, end;
+    struct timespec start, end; /*Holds initial time and final time*/
+    char status;                /*Holds status checking variable*/
 
+    /*Get the starting time*/
     clock_gettime(CLOCK_MONOTONIC, &start);
 
     while(1) {
-        clrscr();
+        /*Clear screen after every iteration of the loop*/
+        clear_screen();
         
+        /*Get the final time*/
         clock_gettime(CLOCK_MONOTONIC, &end);
 
+        /*Find the time elapsed*/
         format_time(end.tv_sec - start.tv_sec);
 
-        sleep(1);
+        /*
+         * 'read' is a system-call (provided by unistd.h)
+
+         * I've configured read to wait only for one second.
+         * If the user enters any character, then it breaks from loop,
+         * else, it returns without waiting any further.
+
+         * Thus, it acts just like 'sleep()' function from 'time.h'
+          
+         * Check function definition of 'enable_raw_mode()'
+         */
+        if(read(STDIN_FILENO, &status, 1) == 1)
+            break;
     }
     
     return 0;
 }
+
+
+void enable_raw_mode(void)
+{
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    
+    /*Enable canonical mode when the program exits*/
+    atexit(disable_raw_mode);
+
+    struct termios raw = orig_termios;
+
+    raw.c_lflag &= ~(ICANON | ECHO); /*Disable canonical mode and input echoing*/
+    raw.c_cc[VTIME] = 1;             /*Set waiting time for 'read()' to be 1 sec*/
+    raw.c_cc[VMIN] = 0;              /*Set min characters to be read by 'read()' as 0*/
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);    
+}
+
+
+void disable_raw_mode(void)
+{
+    /*Restore the original terminal attributes*/
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 
 void format_time(time_t seconds)
 {
@@ -35,8 +96,15 @@ void format_time(time_t seconds)
     hours = minutes / 60;
     minutes = minutes % 60;
 
-    printf("Elapsed time: %.2ld:%.2ld:%.2ld\n", hours, minutes, seconds);
+    printf("======================================\n");
+    printf("\tElapsed time: %.2ld:%.2ld:%.2ld\n", hours, minutes, seconds);
+    printf("======================================\n");
+        
+    printf("\tEnter any key to exit\n");   
 }
 
-void clrscr(void)
-        {printf("\033[H\033[J");}
+
+void clear_screen(void)
+{
+    system("clear");
+}
