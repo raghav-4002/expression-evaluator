bef5ef3b120146c5b788730791ecd538bcb43170
Raghav Sharma <code.practice17@gmail.com>
2025-03-16 15:56:56 +0530

    process basic input and print output

--- shunting-yard-algorithm/main.c
+++ shunting-yard-algorithm/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 
 
 
@@ -18,6 +19,9 @@ int top = -1;
 
 
 void read_input(void);
+void process_input(void);
+void input_to_postfix(void);
+void print_output(void);
 
 
 
@@ -25,6 +29,8 @@ int
 main(void)
 {
     read_input();
+    process_input();
+    print_output();
 
     return 0;
 }
@@ -36,4 +42,44 @@ read_input(void)
 {
     printf("Enter the expression: ");
     fgets(input, ARRAY_SIZE, stdin);
+}
+
+
+
+void
+process_input(void)
+{
+    while(input[input_count] != '\n') {
+        if(isalpha(input[input_count])) {
+            input_to_postfix();
+        }
+    }
+
+    /* add null character at the end */
+    postfix[post_count] = '\0';
+}
+
+
+
+void
+input_to_postfix(void)
+{
+    postfix[post_count] = input[input_count];
+    input_count++;
+    post_count++;    
+}
+
+
+
+void
+print_output(void)
+{
+    post_count = 0;
+
+    while(postfix[post_count] != '\0') {
+        printf("%c", postfix[post_count]);
+        post_count++;
+    }
+    
+    printf("\n");
 }
