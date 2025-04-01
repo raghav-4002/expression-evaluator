b3f747412936db9cb0db75b80cc4072e5e48e0b5
Raghav Sharma <code.practice17@gmail.com>
2025-04-01 17:17:38 +0530

    Added Playability

--- Number Guessing Game/main.c
+++ Number Guessing Game/main.c
@@ -3,17 +3,16 @@
 #include <stdlib.h>
 #include <time.h>
 
+
 #define DIFFICULTY 0
 #define GUESS 1
-
-#define ASCII_ZERO 48 // To check if the user wants to play again
 
 
 void print_message(void);
 void choose_difficulty(void);
-void play(int);
+void play(int attempts);
 int generate_number(void);
-void handle_input(int *, int input_type);
+void handle_input(int *number, int input_type);
 void clear_input_buffer(void);
 
 
@@ -33,6 +32,7 @@ main(void)
         scanf("%d", &choice);
 
         if(choice == 0) break;
+        clear_input_buffer();
     }
 
     return 0;
@@ -70,56 +70,46 @@ choose_difficulty(void)
             case 3:
                 play(5); // Hard: 5 attempts
                 break;
-            }
-
+        }
 }
 
 
-// void play(int attempts)
-// {
-//     int guess, rand_number = generate_number();
-
-//     printf("Guess the number(You have %d attempts): ", attempts);
-
-//     handle_input(&guess);
-
-//     attempts--; // Decrement attempt after first guess
-
-//     while (guess != rand_number && attempts != 0) {
-
-//         if (guess > rand_number)
-//             printf("You guessed a larger number. Try again(%d attempts left): ",
-//                 attempts);
-
-//         else if (guess < rand_number)
-//             printf("You guessed a smaller number. Try again(%d attempts left): ",
-//                 attempts);
-
-//         attempts--;
-
-//         handle_input(&guess);
-//     }
-
-//     if (attempts == 0 && guess != rand_number) { // If user runs out of all attempts
-//         printf("You've run out of all the attempts! The correct number was %d\n",
-//             rand_number);
-
-//         return;
-//     }
-
-//     /*If attempts != 0*/
-//     printf("Correct! The number was indeed %d\n", rand_number);
-// }
-
 void
-play(int a)
+play(int attempts)
 {
-    printf("Playing");
+    int guess, rand_number = 10;
+
+    do {
+        printf("Guess the number (in %d attempts): ", attempts);
+        handle_input(&guess, GUESS);
+
+        if(guess > rand_number) {
+            printf("You've guessed a larger number, try again...\n");
+        } else if(guess < rand_number){
+            printf("You've guessed a smaller number, try again...\n");
+        }
+
+        attempts--;
+    } while(guess != rand_number && attempts != 0);
+
+    if(guess == rand_number) {
+        printf("\nCorrect! The number was indeed %d.\n", rand_number);
+        // printf("It took you %d attempts to guess the correct number\n", attempts + 1);
+
+        return;
+    }
+
+    printf("\nYou've ran out of all the attempts.\n");
+    printf("The correct number was %d\n", rand_number);
 }
 
 
 /*Generate a random number between 1 and 100*/
-int generate_number(void) { return (rand() % 100 + 1); }
+int
+generate_number(void)
+{
+    return (rand() % 100) + 1;
+}
 
 
 /*Handle invalid input*/
@@ -134,12 +124,9 @@ void handle_input(int *num, int input_type)
             printf("Please choose a valid difficulty...\n");
             printf("Try again: ");
 
-            if(term != '\n') {
-                clear_input_buffer();
-            }
+            if(term != '\n') clear_input_buffer();
             term = '\x1b';
         }
-        return;
     }
 
     if(input_type == GUESS) {
