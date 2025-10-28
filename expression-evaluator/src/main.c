b797ee3a85954ee38320be2649bb5f46229b2fc7
Raghav Sharma <code.practice17@gmail.com>
2025-10-28 18:47:15 +0530

    expression-evaluator: Defined main interface; getting unary operation bug

--- expression-evaluator/src/main.c
+++ expression-evaluator/src/main.c
@@ -8,38 +8,58 @@
 #include "utils.h"
 
 
+void
+cleanup(char *input, Token *tokens, Tree_node *ast_root)
+{
+    free(input);
+    free(tokens);
+    free(ast_root);
+
+}
+
+
 int
 main(void)
 {
-    printf("\nEnter the expression: ");
-    char *expression = read_from_stdin();
-    if (!expression) return 0;
+    printf("================================================================\n"
+           "Supported operations:\n"
+        "+ : Addition\n"
+        "- : Subtraction\n"
+        "* : Multiplication\n"
+        "/ : Division\n"
+        "^ : Exponentiation\n"
+        "\nGrouping of numbers via parenthesis is also supported\n"
+        "================================================================\n");
 
-    Token *tokens = tokenize(expression);
-    if (!tokens) {
-        free(expression);
-        return 0;
+    while (1) {
+        /* Reading input */
+        printf("\n> ");
+        char *input = read_from_stdin();
+        if (!input) continue;
+
+        /* Input tokenization */
+        Token *tokens = tokenize(input);
+        if (!tokens) {
+            cleanup(input, NULL, NULL);
+            continue;
+        }
+
+        /* Parsing tokens */
+        size_t current = 0;
+        Tree_node *ast_root = parse_expression(tokens, &current);
+        if (!ast_root) {
+            cleanup(input, tokens, NULL);
+            continue;
+        }
+
+        /* Evaluation and printing of result */
+        if (evaluate_ast(ast_root) == NULL) {
+            cleanup(input, tokens, ast_root);
+            continue;
+        }
+
+        printf("%.3lf\n", ast_root->value);
+
+        cleanup(input, tokens, ast_root);
     }
-
-    size_t current = 0;
-    Tree_node *ast_root = parse_expression(tokens, &current);
-    if (!ast_root) {
-        free(expression);
-        free(tokens);
-        return 0;
-    }
-
-    traverse_tree(ast_root);
-
-    if (evaluate_ast(ast_root) == NULL) {
-        free(expression);
-        free(tokens);
-        return 0;
-    }
-
-    printf("\nResult of expression: %lf\n", ast_root->value);
-
-    free(expression);
-    free(tokens);
-    free(ast_root);
 }
